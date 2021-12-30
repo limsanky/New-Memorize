5e6f4161fb3595396361dc4a61087fdd37cdc162
Sankarshana V <61644684+limsanky@users.noreply.github.com>
2021-12-30 16:18:53 +0900

    lecture 4

--- New Memorize/EmojiMemoryGame.swift
+++ New Memorize/EmojiMemoryGame.swift
@@ -8,23 +8,28 @@
 import SwiftUI
 
 
-class EmojiMemoryGame {
+class EmojiMemoryGame: ObservableObject {
     // We make the two following properties [static], because they are required for any instance of EmojiMemoryGame!
-    static let emojis = ["🚂", "✈️", "🚗", "🚓", "🚡", "🚚", "🚎", "🚑", "⛵️",
+    private static let emojis = ["🚂", "✈️", "🚗", "🚓", "🚡", "🚚", "🚎", "🚑", "⛵️",
                   "🚇", "🛵", "🛬", "🛫", "🛺", "🛰"]
     
-    static func createMemoryGame() -> MemoryGame<String> {
-        MemoryGame<String>(numberOfPairsOfCards: 4) { pairIndex in
+    private static func createMemoryGame() -> MemoryGame<String> {
+        MemoryGame<String>(numberOfPairsOfCards: 3) { pairIndex in
             EmojiMemoryGame.emojis[pairIndex]
         }
     }
     
     // The model is set to [private],
     // because the ViewModel should be the only thing which can see the model.
-    private var model: MemoryGame<String> = EmojiMemoryGame.createMemoryGame()
+    @Published private var model: MemoryGame<String> = EmojiMemoryGame.createMemoryGame()
 
     
     var cards: [MemoryGame<String>.Card] {
         return model.cards
+    }
+    
+    // MARK: Intent(s)
+    func choose(_ card: MemoryGame<String>.Card) {
+        model.choose(card)
     }
 }
